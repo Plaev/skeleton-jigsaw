c460b0bcbf2aada3b18452b062171ff605010e0e
PotHix <pothix@pothix.com>
2012-01-09 14:33:50 -0200

    Updating the update script to use the correct scripts for production and development.

--- tools/update.sh
+++ tools/update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 ./bake.sh
+sed -i 's/<script type="text\/javascript" src="lib\/impact\/impact.js"><\/script>//g' ../index.html
+sed -i 's/<script type="text\/javascript" src="lib\/game\/main.js"><\/script>//g' ../index.html
+sed -i 's/<!-- <script type="text\/javascript" src="game.min.js"><\/script> -->/<script type="text\/javascript" src="game.min.js"><\/script>/g' ../index.html
+
+git checkout ../index.html
+
 scp -r ../index.html ../game.min.js ../media/ plaev@plaevteam.com:/home/plaev/plaevteam.com/skeleton-jigsaw/
