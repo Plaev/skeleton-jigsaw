bf15b84d32c1f989da062bb80b9b6682de11ca97
PotHix <pothix@pothix.com>
2012-01-12 02:02:11 -0200

    Ok mac users...You will have some fun too.

--- tools/update.sh
+++ tools/update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 ./bake.sh
-sed -i 's/<script type="text\/javascript" src="lib\/impact\/impact.js"><\/script>//g' ../index.html
-sed -i 's/<script type="text\/javascript" src="lib\/game\/main.js"><\/script>//g' ../index.html
-sed -i 's/<!-- <script type="text\/javascript" src="game.min.js"><\/script> -->/<script type="text\/javascript" src="game.min.js"><\/script>/g' ../index.html
+if [ -d /Applications ]; then
+	sed -i '' 's/<script type="text\/javascript" src="lib\/impact\/impact.js"><\/script>//g' ../index.html
+	sed -i '' 's/<script type="text\/javascript" src="lib\/game\/main.js"><\/script>//g' ../index.html
+	sed -i '' 's/<!-- <script type="text\/javascript" src="game.min.js"><\/script> -->/<script type="text\/javascript" src="game.min.js"><\/script>/g' ../index.html
+else
+	sed -i 's/<script type="text\/javascript" src="lib\/impact\/impact.js"><\/script>//g' ../index.html
+	sed -i 's/<script type="text\/javascript" src="lib\/game\/main.js"><\/script>//g' ../index.html
+	sed -i 's/<!-- <script type="text\/javascript" src="game.min.js"><\/script> -->/<script type="text\/javascript" src="game.min.js"><\/script>/g' ../index.html
+fi
 
 scp -r ../index.html ../game.min.js ../media/ plaev@plaevteam.com:/home/plaev/plaevteam.com/skeleton-jigsaw/
 git checkout ../index.html
