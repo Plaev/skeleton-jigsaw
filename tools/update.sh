1dae267b8b45cc6c7812a5149ac8463fbcb74bd2
PotHix <pothix@pothix.com>
2012-02-01 13:39:29 -0200

    Changing the directory too.

--- tools/update.sh
+++ tools/update.sh
@@ -11,5 +11,5 @@ else
 	sed -i 's/<!-- <script type="text\/javascript" src="game.min.js"><\/script> -->/<script type="text\/javascript" src="game.min.js"><\/script>/g' ../index.html
 fi
 
-scp -r ../index.html ../game.min.js ../media/ plaevgames@plaevteam.com:/home/plaev/plaevteam.com/skeleton-jigsaw/
+scp -r ../index.html ../game.min.js ../media/ plaevgames@plaevteam.com:/home/plaevgames/plaevteam.com/skeleton-jigsaw/
 git checkout ../index.html
